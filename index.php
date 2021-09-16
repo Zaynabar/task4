a89cd1599d5533ac75d44227f09735b566ae55d1
Zaynabar <90861463+Zaynabar@users.noreply.github.com>
2021-09-17 00:34:20 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <?php
 //include 'config.php';
-?>
-
+/*
 <div class="container">
 <div class="centre">
     <a href="https://www.facebook.com/v12.0/dialog/oauth?client_id=<?=ID?>&redirect_uri=<?=URL?>&response_type=code&scope=public_profile,email" target="_blank">Login with Facebook</a>
 </div>
 </div>
+*/
+?>
+
+
 
 <?php
 include 'index.html';
