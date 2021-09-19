fc16ad695cf79ecba407834be0c3e53136cee484
Maxi <mmaximonchuk@gmail.com>
2021-09-19 22:22:52 +0300

    fix: test

--- db.php
+++ db.php
@@ -1,19 +1,20 @@
 <?php
 
-function showData($link) {
+function showData($link)
+{
 
-    
-     
-    $lastTime = date("Y-m-d");
-    $id = $_SESSION['id'];
-    $query = "UPDATE users SET lastTime = '$lastTime' WHERE social_id = '$id'";
-    mysqli_query($link, $query) or die(mysqli_error($link));
 
-    $query = "SELECT * FROM users";
-    $result = mysqli_query($link, $query) or die(mysqli_error($link));
-    for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);
 
-    $content = '    
+  $lastTime = date("Y-m-d");
+  $id = $_SESSION['id'];
+  $query = "UPDATE users SET lastTime = '$lastTime' WHERE social_id = '$id'";
+  mysqli_query($link, $query) or die(mysqli_error($link));
+
+  $query = "SELECT * FROM users";
+  $result = mysqli_query($link, $query) or die(mysqli_error($link));
+  for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);
+
+  $content = '    
     <div class="filters">
       <select name="socialNetwork" class="filterSocialNetwork">
         <option selected disabled>Выберите социальную сеть</option>
@@ -39,8 +40,8 @@ function showData($link) {
         <th class="status">Status<img src="./down-arrow.svg" /></th>
     </tr></thead><tbody class="tableBody">';
 
-    foreach ($data as $page) {
-        $content .= "<tr>
+  foreach ($data as $page) {
+    $content .= "<tr>
           <td><input onclick='setSessionUserId({$page['social_id']})' type='checkbox' name='check' class='userCheckbox' /></td>
           <td class='userId'>{$page['social_id']}</td>
           <td class='userName'>{$page['name']}</td>
@@ -49,38 +50,41 @@ function showData($link) {
           <td class='lastDate'>{$page['lastTime']}</td>
           <td>{$page['status']}</td>
         </tr>";
-    }
-    $content .= '</tbody></table>';
-
-    include 'layout.php';
+  }
+  $content .= '</tbody></table>';
+  function setSessionUserId($userId)
+  {
+    $_SESSION['id'] = $userId;
+  }
+  include 'layout.php';
 }
 
-function setSessionUserId($userId) {
- $_SESSION['id'] = $userId;
-}
 
-function addData($link, $net) {
-    $id = $_SESSION['id'];
-    $name = $_SESSION['user_first_name'];
-    $socialNetwork = $net;
-    $firstTime = date("Y-m-d");
-    $LastTime = date("Y-m-d");
-    $status = 'active';
 
-    $query = "INSERT INTO users (social_id, name, socialNet, firstTime, lastTime, status) 
+function addData($link, $net)
+{
+  $id = $_SESSION['id'];
+  $name = $_SESSION['user_first_name'];
+  $socialNetwork = $net;
+  $firstTime = date("Y-m-d");
+  $LastTime = date("Y-m-d");
+  $status = 'active';
+
+  $query = "INSERT INTO users (social_id, name, socialNet, firstTime, lastTime, status) 
             VALUES ('$id', '$name', '$socialNetwork', '$firstTime', '$LastTime', '$status')";
-    mysqli_query($link, $query) or die(mysqli_error($link));
+  mysqli_query($link, $query) or die(mysqli_error($link));
 }
 
-function checkData($link) {
-    $id = $_SESSION['id'];
-    $check = mysqli_query($link, "SELECT status FROM users WHERE social_id = '$id'");
-    for ($data = []; $row = mysqli_fetch_assoc($check); $data[] = $row);
-    foreach ($data as $file) {
-        if ($file['status'] == 'blocked') {
-            $_SESSION['alert'] = 'alert';
-            header('location:index.php');
-            die();
-        }
+function checkData($link)
+{
+  $id = $_SESSION['id'];
+  $check = mysqli_query($link, "SELECT status FROM users WHERE social_id = '$id'");
+  for ($data = []; $row = mysqli_fetch_assoc($check); $data[] = $row);
+  foreach ($data as $file) {
+    if ($file['status'] == 'blocked') {
+      $_SESSION['alert'] = 'alert';
+      header('location:index.php');
+      die();
     }
+  }
 }
