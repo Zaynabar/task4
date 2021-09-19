7b91135b302c2c523cf3c9954c23f56cbc5a598b
Ghost Rider <krytou.n@gmail.com>
2021-09-19 15:10:42 +0300

    g

--- config.php
+++ config.php
@@ -20,7 +20,7 @@ $google_client->addScope('email');
 $google_client->addScope('profile');*/
 
 $params = array(
-    'client_id'     => '963788022481-lvothob8d4v2vcecqv2e5a04e0u43vjf.apps.googleusercontent.com',
+    'client_id'     => '963788022481-elled9r3fgmbcnu90ttg9du69jnufv32.apps.googleusercontent.com',
     'client_secret' => 'gFjjCaQDyCzJzn2DYulz7RDk',
     'redirect_uri'  => 'https://authdev.herokuapp.com/google.php',
     'grant_type'    => 'authorization_code',
--- google.php
+++ google.php
@@ -3,34 +3,6 @@ include 'config.php';
 include 'db.php';
 $SocialNetwork = 'Google';
 
-/*
-if (isset($_GET["code"])) {
-    $token = $google_client->fetchAccessTokenWithAuthCode($_GET["code"]);
-
-    if (!isset($token['error'])) {
-        $google_client->setAccessToken($token['access_token']);
-        $_SESSION['access_token'] = $token['access_token'];
-        $google_service = new Google_Service_Oauth2($google_client);
-        $data = $google_service->userinfo->get();
-
-        if (!empty($data['given_name'])) {
-            $_SESSION['user_first_name'] = $data['given_name'];
-        }
-
-        if (!empty($data['family_name'])) {
-            $_SESSION['user_last_name'] = $data['family_name'];
-        }
-
-        if (!empty($data['email'])) {
-            $_SESSION['user_email_address'] = $data['email'];
-        }
-
-        if (!empty($data['id'])) {
-            $_SESSION['id'] = $data['id'];
-        }
-    }
-}*/
-
 if (!empty($_GET['code'])) {
     $ch = curl_init('https://accounts.google.com/o/oauth2/token');
     curl_setopt($ch, CURLOPT_POST, 1);
@@ -66,6 +38,6 @@ if (!(mysqli_num_rows($add) > 0)) {
     addData($link, $SocialNetwork);
 }
 
-checkData($link);
+//checkData($link);
 
 showData($link);
